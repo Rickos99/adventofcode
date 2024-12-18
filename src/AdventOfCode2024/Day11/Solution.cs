cb52d523183ae389e7fd1a85ce8b63f862ab745d
Rickos99 <12218241+Rickos99@users.noreply.github.com>
2024-12-18 23:48:42 +0100

    Add problem for day 11

---
+++ src/AdventOfCode2024/Day11/Solution.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode2024.Day11;
+
+[PuzzleInfo(11, "Plutonian Pebbles")]
+internal sealed class Solution() : Puzzle(11)
+{
+    private readonly static Dictionary<(long stone, int blinks), long> _memo = [];
+
+    public override long SolveFirstPart() => Solve(GetStones(), 25);
+    public override long SolveSecondPart() => Solve(GetStones(), 75);
+
+    private static long Solve(List<long> stones, int blinks)
+    {
+        return stones.Sum(stone => CountStonesAfterBlink(stone, blinks));
+    }
+
+    private static long CountStonesAfterBlink(long stone, int blinks)
+    {
+        if (_memo.TryGetValue((stone, blinks), out var count)) return count;
+        if (blinks == 0) return _memo[(stone, blinks)] = 1;
+
+        count = PlutofyStone(stone).Sum(s => CountStonesAfterBlink(s, blinks - 1));
+        return _memo[(stone, blinks)] = count;
+    }
+
+    private static List<long> PlutofyStone(long stone)
+    {
+        if (stone == 0)
+        {
+            return [1];
+        }
+        else if (IsEvenDigitCount(stone, out var numberOfDigits))
+        {
+            var divider = (int)Math.Pow(10, (numberOfDigits + 1) / 2);
+            return [stone / divider, stone % divider];
+        }
+        else
+        {
+            return [stone * 2024];
+        }
+    }
+
+    private static bool IsEvenDigitCount(long number, out int numberOfDigits)
+    {
+        numberOfDigits = (int)Math.Floor(Math.Log10(number) + 1);
+        return numberOfDigits % 2 == 0;
+    }
+
+    private List<long> GetStones()
+        => _puzzleInput[0].Split(' ').Select(long.Parse).ToList();
+}
